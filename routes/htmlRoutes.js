1c2360ac82c2d9ea5bdbfe23beb0944948508bbc
Amelia Roberts <arob0017@student.monash.edu>
2020-10-15 19:56:27 +1100

    html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+function htmlRoutes(app) {
+    app.get("/tables", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/tables.html"));
+    });
+    app.get("/reserve", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    });
+
+    // sets home as default if routes don't match
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
+module.exports = htmlRoutes(app);
