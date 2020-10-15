516a7f2caf9f2a2c73348e823ddcd9448e544b97
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-10-15 19:48:34 +1100

    apiRoutes module export added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,22 @@
 var reserveTable = require('./data/reserveArray');
 var waitingOnTable = require('./data/waitArray');
 
-// Displays all reserve data
-app.get("/api/reserve", function (req, res) {
-    res.json(reserveTable);
-});
+function apiRoutes(app){
+    // Displays all reserve data
+    app.get("/api/reserve", function (req, res) {
+        res.json(reserveTable);
+    });
+    
+    // Displays all waiting data
+    app.get("/api/waiting", function (req, res) {
+        res.json(waitingOnTable);
+    });
+    
+    // add route
+    
+    // delete route
+    
+    
 
-// Displays all waiting data
-app.get("/api/waiting", function (req, res) {
-    res.json(waitingOnTable);
-});
-
-// add route
-
-// delete route
+}
+module.exports = apiRoutes(app);
