aa48469c64a7abe36f48c17344dda3969de3c27e
Chris Skogstad <chris.skogstad1@gmail.com>
2020-10-15 19:37:42 +1100

    dummy data arrays

---
+++ data/reserveData.js
@@ -0,0 +1,14 @@
+
+
+
+var reserveArray = [
+    {
+        custName: "John Smith",
+        custEmail: "123@fake.com",
+        custPhone: "123 1234 1414",
+        custID: "hasdhfa78"
+    }
+]
+
+
+module.exports = reserveArray;
---
+++ data/waitData.js
@@ -0,0 +1,12 @@
+
+
+var waitArray = [
+    {
+        custName: "Jake Doe",
+        custEmail: "555@fake.com",
+        custPhone: "444 4444 4444",
+        custID: "hfhfh67"
+    }
+]
+
+module.exports = waitArray;
