2371fedec70f4e44fe6bb8cfe6f865658548d1eb
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-10-15 19:53:40 +1100

    apiRoutes simple add added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,33 @@
 var reserveTable = require('./data/reserveArray');
 var waitingOnTable = require('./data/waitArray');
 
-function apiRoutes(app){
+function apiRoutes(app) {
     // Displays all reserve data
-    app.get("/api/reserve", function (req, res) {
+    app.get("/api/reserve", (req, res) => {
         res.json(reserveTable);
     });
-    
+
     // Displays all waiting data
-    app.get("/api/waiting", function (req, res) {
+    app.get("/api/waiting", (req, res) => {
         res.json(waitingOnTable);
     });
-    
+
     // add route
-    
+    app.post("/api/reserve", () => {
+        var newReservation = req.body;
+        
+
+        console.log(newReservation);
+      
+        reserveTable.push(newCharacter);
+      
+        res.json(newReservation);
+    })
+
+
     // delete route
-    
-    
+
+
 
 }
 module.exports = apiRoutes(app);
